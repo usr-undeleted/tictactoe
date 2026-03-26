9fe07622e20207f9603d01d248cb90fda7c390ac
Vaspyyy <lolbautz2@gmail.com>
2026-03-26 19:44:40 +0100

    fixed win condition to not work when player has more than 3 cells

--- code.c
+++ code.c
@@ -14,11 +14,7 @@ char playerChars[2] = "xo";
 const char *error = "\033[31mPlease input a valid cell!\033[0m";
 char errorStr[35] = "";
 
-// scores
-const int cells[9] = {
-    128,    256,    512,
-    16,     32,     64,
-    2,      4,      8};
+int board[9] = {0};
 
 int usedCells[9] = {0};
 
@@ -37,11 +33,6 @@ int main () {
     // the solution of doom #1
     puts("\n\n\n\n\n\n\n");
 
-    // player "scores", basically just the sum of
-    // cells defined in cells[]
-    int playerScores[2] = {0};
-    // 0 is player 1, 1 is player 2
-
     // current player, always at
     // player 1 by default
     int currentPlr = 1;
@@ -51,20 +42,30 @@ int main () {
     for (int i = 0; i < 9; i++) {
         printGrid();
 
+        int winCombinations[8][3] = {
+            {0,1,2}, {3,4,5}, {6,7,8},
+            {0,3,6}, {1,4,7}, {2,5,8},
+            {0,4,8}, {2,4,6}
+        };
+
         // if player1 won
-        if (playerScores[0] == 546 || playerScores[0] == 168 || // diagonal
-            playerScores[0] == 146 || playerScores[0] == 292 || playerScores[0] == 584 || // vertical
-            playerScores[0] == 14 || playerScores[0] == 112 || playerScores[0] == 896) { // horizontal
+        for (int w = 0; w < 8; w++) {
+            if (board[winCombinations[w][0]] == 1 &&
+                board[winCombinations[w][1]] == 1 &&
+                board[winCombinations[w][2]] == 1) {
                 printf("\033[2K\033[33mPlayer 1 has won!\033[0m\n\n");
                 return 1;
+            }
         }
 
         // if player2 won
-        if (playerScores[1] == 546 || playerScores[1] == 168 ||
-            playerScores[1] == 146 || playerScores[1] == 292 || playerScores[1] == 584 ||
-            playerScores[1] == 14 || playerScores[1] == 112 || playerScores[1] == 896) {
+        for (int w = 0; w < 8; w++) {
+            if (board[winCombinations[w][0]] == 2 &&
+                board[winCombinations[w][1]] == 2 &&
+                board[winCombinations[w][2]] == 2) {
                 printf("\033[2K\033[33mPlayer 2 has won!\033[0m\n\n");
                 return 2;
+            }
         }
 
         // the question
@@ -86,7 +87,7 @@ int main () {
 
             if ((input - '0') == j) {
                 slots[j - 1] = playerChars[currentPlr - 1];
-                playerScores[currentPlr - 1] += cells[j - 1];
+                board[j - 1] = currentPlr;
                 usedCells[j - 1] = 1;
 
                 validity = 1;
